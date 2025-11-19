0c5a787d34fc6bf20a9da0b800a08ca0221a118e
walidmohamedfarag <walid492003@gmail.com>
2025-11-19 16:35:18 +0200

    Add project files.

---
+++ BookStoreAPIs/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStoreAPIs.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Author { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public decimal Price { get; set; }
+        public double Discount { get; set; }
+    }
+}
