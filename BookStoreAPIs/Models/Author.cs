a6455ee35b9a70582f481c1996bab89c880518fa
walidmohamedfarag <walid492003@gmail.com>
2025-11-22 16:35:27 +0200

    add relation between author and book

--- BookStoreAPIs/Models/Author.cs
+++ BookStoreAPIs/Models/Author.cs
@@ -1,9 +1,13 @@
-﻿namespace BookStoreAPIs.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace BookStoreAPIs.Models
 {
     public class Author
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string BirthDate { get; set; } = string.Empty;
+        public int BookId { get; set; }
+        public Book Book { get; set; } = null!;
     }
 }
