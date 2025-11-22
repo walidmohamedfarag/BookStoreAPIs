a6eb911a4ae93604a95185dc952c512d936dc0e0
walidmohamedfarag <walid492003@gmail.com>
2025-11-22 12:56:57 +0200

    delete author prop from book table and add author table

--- BookStoreAPIs/Models/Book.cs
+++ BookStoreAPIs/Models/Book.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public string Author { get; set; } = string.Empty;
         public DateTime Created { get; set; }
         public decimal Price { get; set; }
         public double Discount { get; set; }
