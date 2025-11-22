53de8b817ff0358d6cb4ef5a188bc74ff6098b9b
walidmohamedfarag <walid492003@gmail.com>
2025-11-22 16:33:17 +0200

    add book controller

---
+++ BookStoreAPIs/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPIs.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
